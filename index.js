d6f3ac14c598941c95d079157f6b96ba0f1c28ee
Michael Hancock <shanehancock64@live.com>
2022-06-04 22:20:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2'); 
 const cTable = require('console.table');
 const { append } = require('express/lib/response');
+const res = require('express/lib/response');
 const app = express();
 //connection 
 const db = mysql.createConnection({
@@ -17,19 +18,31 @@ db.connect(err => {
   console.log('Great success connected to Mysql!');
 });
 // create database
-db.query("CREATE DATABASE employeeTracker", function (err, result) {
-  if (err) throw err;
-  console.log("Database created");
+app.get('/createdb', (req, res) => {
+  let sql = "CREATE DATABASE employeeTracker"
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send("Database created");
+})
+
 });
 
 // create employee table
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "employeeTracker"
+});
+
+
 db.connect(function(err) {
   if (err) throw err;
-  console.log("Connected to Table!");
+  res.send("Connected to Table!");
   var sql = 'CREATE TABLE Employee (id INT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT, manager_id INT)';
   db.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    res.send("Table created");
   });
 });
 // app.get('createemployee', (req, res) => {
