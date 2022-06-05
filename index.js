4fbb6cd2766d01e320878a6ac4e9abb33d4a9c08
Michael Hancock <shanehancock64@live.com>
2022-06-04 21:53:37 -0700

    port listener

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2'); 
 const cTable = require('console.table');
-
+const { append } = require('express/lib/response');
+const app = express();
 //connection 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -20,3 +21,11 @@ db.query("CREATE DATABASE employeeTracker", function (err, result) {
   if (err) throw err;
   console.log("Database created");
 });
+
+
+
+
+// Port Listening
+app.listen('3001', () => {
+  console.log("Listening on port 3001 my guy!");
+})
