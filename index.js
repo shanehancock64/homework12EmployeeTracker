2359b4caeab9026c9b01a37b87aea0492a6b6767
Michael Hancock <shanehancock64@live.com>
2022-06-05 18:26:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -334,7 +334,84 @@ function getUpdatedRole(employee, roleChoices) {
 }
 
 // adding role 
+function addRole(){
+  let sql = 
+  `SELECT 
+    department.id, 
+    department.name, 
+    role.salary
+  FROM employee
+  JOIN role
+    ON employee.role_id = role.id
+  JOIN department
+    ON department.id = role.department_id
+  GROUP BY department.id, department.name`
 
+  db.query(sql,(err, res) => {
+    if(err){
+      throw err;
+    }
+    const department = res.map(({ id, name }) => ({
+      value: id,
+      name: `${id} ${name}`
+    }));
+    console.table(res);
+    addToRole(department);
+  });
+}
+
+function addToRole(department){
+  inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Role title: "
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Role Salary: "
+      },
+      {
+        type: "list",
+        name: "department",
+        message: "Department: ",
+        choices: department
+      },
+    ]).then((res)=>{
+      let sql = `INSERT INTO role SET ?`;
+
+      db.query(sql, {
+          title: res.title,
+          salary: res.salary,
+          department_id: res.department
+      },(err, res) => {
+          if(err){
+            throw err;
+          } 
+          startApp();
+      });
+  });
+}
+
+// adding department
+function addDepartment(){
+inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: " Name of Department: "
+    }
+  ]).then((res)=>{
+  let sql = `INSERT INTO department SET ?`;
+  db.query(sql, {name: res.name},(err, res) => {
+    if(err){
+      throw err;
+    } 
+    startApp();
+  });
+});
+}
 
 
 
